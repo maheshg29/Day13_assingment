ef5a97d0a2a92320a25ed7617afd965b644cb9a0
mahesh <gangurdemahesh9@gmail.com>
2022-10-28 23:33:07 +0530

    [mahesh]Add Main Program file

---
+++ Day13PracticeProblem/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Day13PracticeProblem
+{
+    class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Wlcome To Day 13 Practice Problem");
+            int firstnum = 55, secondnum = 90, thirdnum = 155;
+            double firstnum1 = 5.5, secondnum1 = 88.5, thirdnum1 =10.5;
+            string firstnum2 = "Apple", secondnum2 = "Peach", thirdnum2 = "Banana";
+
+            int num = GenericMax<int>.Maxnum(firstnum, secondnum, thirdnum);
+            double num1 = maxfloat<double>.Maxnum(firstnum1, secondnum1, thirdnum1);
+            string num2 = maxstring<string>.Maxnum(firstnum2, secondnum2, thirdnum2);
+
+            Console.WriteLine(num);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+
+        }
+    }
+}
