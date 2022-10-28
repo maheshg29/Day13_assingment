b5ba8b7a07ca6b0c2c7d3692c63666e0ebacc8d3
mahesh <gangurdemahesh9@gmail.com>
2022-10-28 23:37:45 +0530

    [mahesh]add UC1 file for integer max number

---
+++ Day13PracticeProblem/GenericMax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13PracticeProblem
+{        public class GenericMax<T> where T : IComparable
+        {
+            public T first, second, third;
+
+            public GenericMax(T first, T second, T third)
+            {
+                this.first = first;
+                this.second = second;
+                this.third = third;
+            }
+
+            public static int Maxnum(int first, int second, int third)
+            {
+
+                if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+                {
+                    return first;
+                }
+
+                if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+                {
+                    return second;
+                }
+
+                if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
+                    third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
+                    third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+                {
+                    return third;
+                }
+               
+            return first;
+
+            }
+
+     
+        }
+    
+}
