a3d489928b1a9c42881c29cec534ff6e3b033c73
mahesh <gangurdemahesh9@gmail.com>
2022-10-28 23:38:59 +0530

    [mahesh]add UC2 file for float max number

---
+++ Day13PracticeProblem/maxfloat.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13PracticeProblem
+{
+    internal class maxfloat<T> where T : IComparable
+    {
+        
+            public T first, second, third;
+
+            public maxfloat(T first, T second, T third)
+            {
+                this.first = first;
+                this.second = second;
+                this.third = third;
+            }
+
+            public static double Maxnum(double first, double second, double third)
+            {
+
+                if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+                {
+                    return first;
+                }
+
+                if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+                {
+                    return second;
+                }
+
+                if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0 ||
+                    third.CompareTo(first) >= 0 && third.CompareTo(second) > 0 ||
+                    third.CompareTo(first) > 0 && third.CompareTo(second) >= 0)
+                {
+                    return third;
+                }
+
+                return first;
+
+            }
+    }
+
+
+}
